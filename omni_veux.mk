4d0434a80aafede0538fcd29cd4c9fb86180c4c5
darkraven123 <107791668+darkraven123@users.noreply.github.com>
2022-11-21 13:43:52 +0530

    Update omni_veux.mk

--- omni_veux.mk
+++ omni_veux.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
 # Inherit common product files.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
